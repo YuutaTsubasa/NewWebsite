6977840c3747a5aedf464225d0b0b273d4764a35
Yuuta Tsubasa <yuuta.tsubasa.knight@gmail.com>
2023-04-08 16:33:24 +0800

    Finish Theme.

---
+++ input/scripts/main.js
@@ -0,0 +1,62 @@
+const CURRENT_TAG_CLASS = "current";
+
+function getTagComponent(tag){
+    return `<li>${tag}</li>`;
+}
+
+function getTagsComponent(tags){
+    let result = "";
+    for(let tag of tags){
+        result += getTagComponent(tag);
+    }
+    return result;
+}
+
+function getPostComponent(post){
+    return `<div class="post">` +
+        `<a href='${post.Link}' class="thumbnail" style='background-image: url("${post.Thumbnail}")'></a>` +
+        `<h3>${post.Published}－${post.Author}</h3>` +
+        `<ul>${getTagsComponent(post.Tags)}</ul>` +
+        `<h2><a href='${post.Link}'>${post.Title}</a></h2>` +
+        `</div>`;
+}
+
+function printPosts(posts){
+    let postList = document.querySelector(".post_list");
+
+    postList.innerHTML = "";
+    for(let post of posts){
+        postList.innerHTML += getPostComponent(post);
+    }
+}
+
+
+document.addEventListener("DOMContentLoaded", function(){
+    let tags = document.querySelectorAll("nav.tags li");
+    if (tags.length > 0){
+        let currentTag;
+        function changeTag(selectedTag){
+            currentTag = selectedTag.innerText;
+            for(let tag of tags){
+                tag.classList.remove(CURRENT_TAG_CLASS);
+            }
+            selectedTag.classList.add(CURRENT_TAG_CLASS);
+
+            printPosts(posts);
+        }
+        changeTag(tags[0]);
+
+        for(let tag of tags) {
+            tag.addEventListener("click", function(event){
+                changeTag(event.target)
+            });
+        }
+    }
+
+    let header = document.querySelector("header");
+    if(header){
+        header.addEventListener("click", function(event){
+            location.assign("/");
+        });
+    }
+});
